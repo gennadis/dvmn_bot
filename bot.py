d746873ef5db5d4af08ed6322e667d0fc6909755
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-07 15:28:40 +0300

    Add reviews long polling

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import os
+import time
 from pprint import pprint
 
 import requests
 from dotenv import load_dotenv
 
 DVMN_REVIEWS_URL = "https://dvmn.org/api/user_reviews/"
+DVMN_LONGPOLLING_URL = "https://dvmn.org/api/long_polling/"
 
 
 def get_reviews(token: str):
@@ -15,6 +17,20 @@ def get_reviews(token: str):
     return response.json()
 
 
+def get_long_polling(token: str):
+    headers = {"Authorization": f"Token {token}"}
+    # params = {"timestamp": time.time()}
+    while True:
+        response = requests.get(DVMN_LONGPOLLING_URL, headers=headers)
+        response_object = response.json()
+
+        if response_object["status"] == "found":
+            pprint(response_object)
+            continue
+        else:
+            pprint(response_object)
+
+
 def main():
     pass
 
@@ -23,4 +39,5 @@ if __name__ == "__main__":
     load_dotenv()
     token = os.getenv("DVMN_TOKEN")
 
-    pprint(get_reviews(token))
+    # pprint(get_reviews(token))
+    get_long_polling(token)
